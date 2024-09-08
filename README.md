6247297aaf9b2e82c1fc69b468b9182b4d176dea
Nurbek333 <nurbekuktamov333@gmail.com>
2024-09-08 14:44:33 +0500

    com

---
+++ README.md
@@ -0,0 +1,1 @@
+Maktab uchun veb sayt
---
+++ main/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from main.models import *
+
+# Register your models here.
+admin.site.register(BestPupil)
+admin.site.register(Teacher)
+admin.site.register(Blog)
+admin.site.register(Home)
+admin.site.register(Pupil)
+admin.site.register(Grade)
